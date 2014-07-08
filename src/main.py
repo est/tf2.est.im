4a6b98e38a880e12b357c3021cd17dfa8e51cd30
est <electronicstar@126.com>
2014-07-08 21:15:23 +0800

    server emitter

--- src/main.py
+++ src/main.py
@@ -1,15 +1,22 @@
 #!/usr/bin/env python
-
 # coding: utf8
 
+from gevent import monkey, spawn, sleep
+monkey.patch_all()
+
 import valve.source.master_server
+import valve.source.a2s
 
 def get_all():
     msq = valve.source.master_server.MasterServerQuerier()
     for address in msq.find(region=u'all',
                         gamedir="tf",
                         map="ctf_2fort"):
-        print "{0}:{1}".format(*address)
+        
+        server = valve.source.a2s.ServerQuerier(address)
+        info = server.get_info()
+        fields = ['server_name', 'map', 'player_count', 'max_players', 'bot_count', 'password_protected']
+        print  address, '\t'.join(info[f] for f in fields)
 
 
 
