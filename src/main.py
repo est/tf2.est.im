884b48513a8c6e585362d083b3d80b34e5bca68b
est <electronicstar@126.com>
2014-07-08 21:09:45 +0800

    server emitter

--- src/main.py
+++ src/main.py
@@ -5,7 +5,12 @@
 import valve.source.master_server
 
 def get_all():
-	pass
+    msq = valve.source.master_server.MasterServerQuerier()
+    for address in msq.find(region=u'all',
+                        gamedir="tf",
+                        map="ctf_2fort"):
+        print "{0}:{1}".format(*address)
+
 
 
 
@@ -17,3 +22,5 @@ if '__main__' == __name__:
 
     __import__('BaseHTTPServer').BaseHTTPRequestHandler.address_string = lambda x:x.client_address[0]
     # run(application, host='0.0.0.0', port=8002, reload=True)
+
+    get_all()
