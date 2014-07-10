031fd97b4b8547caaf9760c45aa18e673fb18346
est <electronicstar@126.com>
2014-07-10 22:01:37 +0800

    master server query finder

--- src/main.py
+++ src/main.py
@@ -1,44 +1,43 @@
 #!/usr/bin/env python
 # coding: utf8
-import time
+import time, struct
 
 from gevent import monkey, spawn, sleep, socket
 monkey.patch_all()
 
-import valve.source.master_server
-import valve.source.a2s
 
-class GBaseServer(object): #valve.source.a2s.BaseServerQuerier):
+S0 = "hl2master.steampowered.com:27011"
+S1 = "208.64.200.52:27011"
+S2 = "208.64.200.39:27011"
 
-    def __init__(self, address, timeout=5.0):
-        self.host = address[0]
-        self.port = address[1]
-        self.timeout = timeout
-        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.socket.settimeout(self.timeout)
+def query_master_server(master_addr=("208.64.200.52", 27011)):
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.settimeout(5)
+    last_addr = ('0.0.0.0', 0)
+    while 1:
+        s.sendto('1\xFF%s:%s\0\0' % (last_addr[0], last_addr[1]), master_addr)
+        b = s.recv(1400)
+        if b.startswith('\xFF\xFF\xFF\xFF\x66\x0A'):
+            i = 6
+        else:
+            i = 0
+        while i<len(b):
+            saddr = b[i:i+6]
+            last_addr = socket.inet_ntoa(b[i:i+4]), struct.unpack('!H', b[i+4:i+6])[0]
+            yield last_addr
+            i += 6
 
-    def request(self, request):
-        self.socket.sendto(request.encode(), (self.host, self.port))
-
-    def get_response(self):
-        return self.socket.recv(1400)
-
-class GMasterServer(GBaseServer, valve.source.master_server.MasterServerQuerier):
-    
-    def __init__(self, address=valve.source.master_server.MASTER_SERVER_ADDR, timeout=10.0):
-        super(GMasterServer, self).__init__(address, timeout)
 
 
-class GServerQuerier(GBaseServer, valve.source.a2s.ServerQuerier):
-    pass
+
 
 def get_all():
-    msq = GMasterServer()
+    msq = valve.source.master_server.MasterServerQuerier()
     for address in msq.find(region=u'all',
                         gamedir="tf",
                         map="ctf_2fort"):
         
-        server = GServerQuerier(address)
+        server = valve.source.a2s.ServerQuerier(address)
         t1 = time.time()
         info = server.get_info()
         ttl = time.time() - t1
@@ -58,10 +57,11 @@ def get_all():
 
 if '__main__' == __name__:
     
-    import readline, rlcompleter; readline.parse_and_bind("tab: complete")
+    # import readline, rlcompleter; readline.parse_and_bind("tab: complete")
 
 
-    __import__('BaseHTTPServer').BaseHTTPRequestHandler.address_string = lambda x:x.client_address[0]
+    # __import__('BaseHTTPServer').BaseHTTPRequestHandler.address_string = lambda x:x.client_address[0]
     # run(application, host='0.0.0.0', port=8002, reload=True)
 
-    get_all()
+    for a in query_master_server():
+        print a
