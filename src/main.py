a86e6584beceabd2ced36c633a6f53a16980654b
est <electronicstar@126.com>
2014-07-10 21:27:31 +0800

    gevent test

--- src/main.py
+++ src/main.py
@@ -2,19 +2,43 @@
 # coding: utf8
 import time
 
-from gevent import monkey, spawn, sleep
+from gevent import monkey, spawn, sleep, socket
 monkey.patch_all()
 
 import valve.source.master_server
 import valve.source.a2s
 
+class GBaseServer(object): #valve.source.a2s.BaseServerQuerier):
+
+    def __init__(self, address, timeout=5.0):
+        self.host = address[0]
+        self.port = address[1]
+        self.timeout = timeout
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.socket.settimeout(self.timeout)
+
+    def request(self, request):
+        self.socket.sendto(request.encode(), (self.host, self.port))
+
+    def get_response(self):
+        return self.socket.recv(1400)
+
+class GMasterServer(GBaseServer, valve.source.master_server.MasterServerQuerier):
+    
+    def __init__(self, address=valve.source.master_server.MASTER_SERVER_ADDR, timeout=10.0):
+        super(GMasterServer, self).__init__(address, timeout)
+
+
+class GServerQuerier(GBaseServer, valve.source.a2s.ServerQuerier):
+    pass
+
 def get_all():
-    msq = valve.source.master_server.MasterServerQuerier()
+    msq = GMasterServer()
     for address in msq.find(region=u'all',
                         gamedir="tf",
                         map="ctf_2fort"):
         
-        server = valve.source.a2s.ServerQuerier(address)
+        server = GServerQuerier(address)
         t1 = time.time()
         info = server.get_info()
         ttl = time.time() - t1
