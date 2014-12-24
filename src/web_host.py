ae4dbc094c0e8bcbbe89e5236a24d95056569f3d
est <electronicstar@126.com>
2014-12-24 23:35:23 +0800

    initial SSE version

--- src/web_host.py
+++ src/web_host.py
@@ -1,68 +1,32 @@
 #!/usr/bin/env python
 #coding: utf8
 
-import time
+import json
 
-from gevent import monkey; monkey.patch_all()
+from gevent import monkey, pool, sleep
+monkey.patch_all()
 
 # https://gist.github.com/werediver/4358735
 from bottle import get, post, request, response
 from bottle import GeventServer, run
 
-
 from main import query_master_server, query_server
 
+addr_gen = query_master_server()
 
-sse_test_page = """
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="UTF-8" />
-        <script src="http://cdnjs.cloudflare.com/ajax/libs/jquery/1.8.3/jquery.min.js "></script>
-        <script>
-            $(document).ready(function() {
-                var es = new EventSource("stream");
-                es.onmessage = function (e) {
-                    $("#log").html($("#log").html()
-                        + "<p>Event: " + e.event + ", data: " + e.data + "</p>");
-                };
-            })
-        </script>
-    </head>
-    <body>
-        <div id="log" style="font-family: courier; font-size: 0.75em;"></div>
-    </body>
-</html>
-"""
- 
- 
 @get('/')
 def index():
-    return sse_test_page
- 
- 
-@get('/stream')
-def stream():
-    # "Using server-sent events"
-    # https://developer.mozilla.org/en-US/docs/Server-sent_events/Using_server-sent_events
-    # "Stream updates with server-sent events"
-    # http://www.html5rocks.com/en/tutorials/eventsource/basics/
- 
+    return open('index.html', 'r').read()
+
+@get('/enum_server')
+def enum_server():
     response.content_type  = 'text/event-stream'
     response.cache_control = 'no-cache'
+    yield 'retry: 1000\n\n'
+    r = addr_gen.next()
+    print r
+    if r:
+        yield 'data: %s' % json.dumps(query_server(r))
  
-    # Set client-side auto-reconnect timeout, ms.
-    yield 'retry: 100\n\n'
- 
-    n = 1
- 
-    # Keep connection alive no more then... (s)
-    end = time.time() + 60
-    while time.time() < end:
-        yield 'data: %i\n\n' % n
-        n += 1
-        time.sleep(1)
- 
- 
-if __name__ == '__main__':
+if '__main__' == __name__:
     run(server=GeventServer)
